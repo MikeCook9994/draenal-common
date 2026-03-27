f6bdc4004ec3e2bb81bc9b8eac750b4513330e45
Michael Cook <you@example.com>
2026-03-27 16:39:53 -0700

    implement applying effect when another effect is enabled

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -13,9 +13,32 @@ CONFIG.debug.hooks = true;
         // dnd5e.dataModels.spellcasting.SpellcastingModel.TYPES.point = PointSpellcasting;
     });
 
-    Hooks.on("preCreateItem", async (document, data, options, userId) => {
-        console.log("preCreateItem", { document, data, options, userId });
-    })
+    Hooks.on('updateActiveEffect', async (effect, changes, options, userId) => {
+        const actor = effect.target;
+        const feature = actor.sourcedItems.get("Compendium.draenal-common.classes.Item.J9dHLyG43bYxmNC5");
+        if (!effect.origin.endsWith(".ActiveEffect.G5XZTi4zYTFiHVll") && feature?.size) {
+            return;
+        }
+
+        // Check for existing effect. Origin is the base cast and smash effect. That varies
+        // Id varies, so you check against the origin which uses the source effect id
+        const existingEffect = actor.effects.find(e => e.origin.endsWith(".ActiveEffect.iT5HADHVwWSvXjWv")); 
+        if (!existingEffect) {
+            const baseEffect = feature.first().effects.contents[0];
+            const effectData = {
+                ...baseEffect,
+                origin: baseEffect.uuid,
+            }
+
+            await ActiveEffect.create(effectData, {
+                parent: actor
+            });
+        }
+
+        await existingEffect.update({
+            disabled: changes.disabled
+        });
+    });
 
     // Hooks.once('init', PointSpellcasting.Init);
     // Hooks.once('init', VitalMagic.Init);
